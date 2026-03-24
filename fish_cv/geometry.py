0c10fbd59be867bd0983604e53a4f9e3a8464572
bjornivar <bihoie@stud.ntnu.no>
2026-03-24 13:53:59 +0100

    feat: add function to find leftmost and rightmost fish points

---
+++ fish_cv/geometry.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def find_left_right_points(mask):
+    """Returns the coordinates for the farthest point to the left and right of the fish"""
+    ys, xs = np.nonzero(mask > 0)
+
+    if len(xs) == 0:
+        raise ValueError("No segmented object found")
+
+    min_x = xs.min()
+    left_y = ys[xs == min_x].min()
+
+    max_x = xs.max()
+    right_y = ys[xs == max_x].min()
+
+    left = (int(min_x), int(left_y))
+    right = (int(max_x), int(right_y))
+    return left, right
