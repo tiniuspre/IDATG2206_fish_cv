20ba3904033f946952a11844ddbad3269e0a510c
bjornivar <bihoie@stud.ntnu.no>
2026-03-24 13:52:18 +0100

    feat: add constants class

---
+++ fish_cv/constants.py
@@ -0,0 +1,9 @@
+INPUT_PATH = "data/fish/fishtestimg.jpg"
+OUTPUT_PATH = "data/result/result.png"
+
+H_B = 1504 #Reference points length in pixels
+L_B = 42 #Real size of reference points
+D_A = 48 #Distance between camera and the fish
+D_B = 58.6 #Distance between the camera and the reference object
+
+KERNEL_SIZE = 7
