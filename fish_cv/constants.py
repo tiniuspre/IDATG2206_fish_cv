eed26cef29309f723f2efc775cb173d6d44582e1
bjornivar <bihoie@stud.ntnu.no>
2026-04-06 16:06:33 +0200

    Updated constants.py

--- fish_cv/constants.py
+++ fish_cv/constants.py
@@ -1,9 +1,11 @@
 INPUT_PATH = "data/fish/fishtestimg.jpg"
 OUTPUT_PATH = "data/result/result.png"
 
-H_B = 1504 #Reference points length in pixels
-L_B = 42 #Real size of reference points
-D_A = 48 #Distance between camera and the fish
-D_B = 58.6 #Distance between the camera and the reference object
+H_B = 159 #Reference points length in pixels
+L_B = 5 #Real size of reference points
+D_B = 65.8 #Distance between the camera and the reference object
+D_A = 48.2 #Distance between camera and the blue fish
+# Orange fish distance was measured as 47.5
+# Height from camera to fish tank bottom 17.5
 
 KERNEL_SIZE = 7
