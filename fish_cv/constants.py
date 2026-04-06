56a0a2cd77b93b184c526c0b1414dacbc510321a
bjornivar <bihoie@stud.ntnu.no>
2026-04-06 18:40:24 +0200

    fix ruff

--- fish_cv/constants.py
+++ fish_cv/constants.py
@@ -7,5 +7,4 @@ D_B = 65.8 #Distance between the camera and the reference object
 D_A = 48.2 #Distance between camera and the blue fish
 # Orange fish distance was measured as 47.5
 # Height from camera to fish tank bottom 17.5
-
 KERNEL_SIZE = 7
