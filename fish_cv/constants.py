16bc19e8709a43c287ee80fa334e1ca3679c05c5
Bjørn Ivar Thorsen Høie <bjornivar.hoie@gmail.com>
2026-04-08 17:20:46 +0200

    Fix main (#21)
    * fixed constants
    
    * Removed old files
    
    * Added the correct image

--- fish_cv/constants.py
+++ fish_cv/constants.py
@@ -1,12 +1,13 @@
-INPUT_PATH = 'data/fish/fishtestimg.jpg'
+INPUT_PATH = 'data/fish/fishIncline.JPG'
 OUTPUT_PATH = 'data/result/result.png'
 
 # IMG CONSTS
-H_B = 159  # Reference points length in pixels
+H_B = 268  # Reference points length in pixels
 L_B = 5  # Real size of reference points
 D_B = 65.8  # Distance between the camera and the reference object
-D_A = 48.2  # Distance between camera and the blue fish
-# Orange fish distance was measured as 47.5
+D_A = 52.7  # Distance between camera and the salmon incline
+# Salmon normal position fish distance was measured as 47.5
+# Trout fish distance was measured as 47.5
 # Height from camera to fish tank bottom 17.5
 
 # VIDEO CONSTS
